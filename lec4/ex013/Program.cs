5928b29274803a2f3d09b740e69341506fc3193a
Denis <lost.ned@gmail.com>
2022-12-11 21:04:45 +0300

    ad ex013-14

--- lec4/ex013/Program.cs
+++ lec4/ex013/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[,] table = new string[2,5];
+// String.Empty
+// table[0, 0] table[0, 1] table[0, 2] table[0, 4]
+// table[1, 0] table[1, 1] table[1, 2] table[1, 4]
+
+
+// table[1, 2] = "Слово"; 
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.WriteLine($"{table[rows, columns]}");
+//     }
+// }
+
+
+
+void PrintArray(int [,] matr) // Метод создания матрицы
+{
+    for (int i = 0; i < matr.GetLength(0); i++) // i < matrix.GetLenght(0). Этот цикл для строк
+    {
+        for( int j = 0; j < matr.GetLength(1); j++) // j < matrix.GetLength(1). Этот для столбцов
+        {
+            Console.Write($"{matr[i, j]} ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for( int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);// [1,10]
+        }
+    }
+    
+}
+
+int [,] matrix =  new int[3,4];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
